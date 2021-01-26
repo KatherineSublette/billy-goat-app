aafe022b8a04ebb17c276bdbf1f5437648efcbcf
KatherineSublette <katherine.sublette@hotmail.com>
2021-01-25 17:38:35 -0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'slider-app'`, () => {
+  it(`should have as title 'billy-goats-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('slider-app');
+    expect(app.title).toEqual('billy-goats-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to slider-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to billy-goats-app!');
   });
 });
