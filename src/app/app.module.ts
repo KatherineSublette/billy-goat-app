e890018e5f35bcf985fc6f10f232193f97794638
KatherineSublette <katherine.sublette@hotmail.com>
2021-02-08 15:29:19 -0700

    sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,4 +30,8 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  onSignUp(): void{
+    this.router.navigateByUrl('/sign-up');
+  }
+
 }
