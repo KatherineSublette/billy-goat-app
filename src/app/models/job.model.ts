737dfc5e697914cd9f8ca5507456b739e5fff898
KatherineSublette <katherine.sublette@hotmail.com>
2021-04-18 21:54:06 -0600

    working accept messages

--- src/app/models/job.model.ts
+++ src/app/models/job.model.ts
@@ -1,9 +1,16 @@
+import { Guest } from "./guest.model";
+import { Guide } from "./guide.model";
+import { Resort } from "./resort.model";
+
 export class Job {
     id: number;
     name: string;
     date: Date;
     completed: boolean;
-    guestid: number;
-    guideid: number;
-    resortid: number;
+    guestId: number;
+    guideId: number;
+    resortId: number;
+    guest: Guest;
+    guide: Guide;
+    resort: Resort;
 }
--- src/app/shared/open-job-page/open-job-page.component.ts
+++ src/app/shared/open-job-page/open-job-page.component.ts
@@ -6,6 +6,7 @@ import { User } from 'src/app/models/user.model';
 import { GuestService } from 'src/app/services/guest.service';
 import { GuideService } from 'src/app/services/guide.service';
 import { JobService } from 'src/app/services/job.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home-page',
@@ -17,22 +18,50 @@ export class OpenJobPageComponent implements OnInit {
   @Input() display: string;
   private jwtUser: User = JSON.parse(sessionStorage.getItem("user"));
   private user: User;
-  private guest: Guest;
-  private jobs: any[];
-  private displayedColumns: string[] = ['name', 'resort', 'guest', 'date'];
-  constructor( private jobService: JobService, private guideService: GuideService, private guestService: GuestService) { 
+  private guide: Guide;
+  private jobs: Job[] = [];
+  private displayedColumns: string[] = ['name', 'resort', 'guest', 'date', 'accept'];
+  constructor( private jobService: JobService, private guideService: GuideService, private toastr: ToastrService) { 
     this.user = JSON.parse(this.jwtUser.user);
+    this.guideService.getGuideByUserId(this.user.id).subscribe(
+      (guide: Guide) => {
+        console.log(guide);
+        this.guide = guide[0];
+      }
+    );
   }
 
   ngOnInit(): void {  
     console.log(this.display);
     if(this.display === "jobs"){
-      this.jobService.getJobs("?guideId=null").subscribe(
-        (jobs: Job[]) => {
-          this.jobs = jobs
-        }
-      );
+      this.refreshJobs();
     }
+  }
+
+  onAccept(job: Job): void {
+    console.log(job);
+    console.log(this.guide);
+    job.guideId = this.guide.id;
+    job.guest = null;
+    job.guide = null;
+    job.resort = null;
+    this.jobService.updateJob(job).subscribe(
+      (job: Job) => {
+        this.toastr.success('You have accepted this job');
+        this.refreshJobs();
+      }
+    );
+    (error) => {
+      this.toastr.error('Error updating guest.');
+    }
+  }
+
+  refreshJobs(): void {
+    this.jobService.getJobs("?guideId=null").subscribe(
+      (jobs: Job[]) => {
+        this.jobs = jobs
+      }
+    );
   }
 
 
